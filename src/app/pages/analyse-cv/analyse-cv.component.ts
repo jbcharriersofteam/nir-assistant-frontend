391803aafad96e28d5479959563e0837da04b6e9
jbcharrier <jbcharrier33@gmail.com>
2024-09-24 11:21:26 +0200

    analyse-cv in progress

--- src/app/pages/analyse-cv/analyse-cv.component.ts
+++ src/app/pages/analyse-cv/analyse-cv.component.ts
@@ -2,14 +2,36 @@ import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { TagModule } from 'primeng/tag';
 import { RatingModule } from 'primeng/rating';
+import { DetailsCardComponent } from '../../shared/details-card/details-card.component';
+import { DetailsCard } from '../../core/models/model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-analyse-cv',
   standalone: true,
-  imports: [CardModule, TagModule, RatingModule],
+  imports: [CardModule, TagModule, RatingModule, DetailsCardComponent, CommonModule],
   templateUrl: './analyse-cv.component.html',
   styleUrl: './analyse-cv.component.css'
 })
 export class AnalyseCvComponent {
+
+  detailsData: DetailsCard[] = [{
+    icon: 'pi-thumbs-up',
+    title: 'Points forts :',
+    subtitle: 'Expérience variée dans différents secteurs (médias, leasing..)',
+    length: 6
+  },
+  {
+    icon: 'pi-chart-line',
+    title: 'Points d’amélioration :',
+    subtitle: 'Renforcer les compétences en dataviz, acquérir des certifications...',
+    length: 6
+  },
+  {
+    icon: 'pi-list-check',
+    title: 'Recommandations :',
+    subtitle: 'Mettre en avant les projets cloud/Data les plus significatifs ...',
+    length: 12
+  }];
 
 }
