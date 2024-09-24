07f7146d26042818e83a6c852e9e218cb4bdfe0e
jbcharrier <jbcharrier33@gmail.com>
2024-09-24 17:12:36 +0200

    Analyse cv integration done

--- src/app/pages/analyse-cv/analyse-cv.component.ts
+++ src/app/pages/analyse-cv/analyse-cv.component.ts
@@ -5,11 +5,13 @@ import { RatingModule } from 'primeng/rating';
 import { DetailsCardComponent } from '../../shared/details-card/details-card.component';
 import { DetailsCard } from '../../core/models/model';
 import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-analyse-cv',
   standalone: true,
-  imports: [CardModule, TagModule, RatingModule, DetailsCardComponent, CommonModule],
+  imports: [CardModule, TagModule, RatingModule, DetailsCardComponent, CommonModule, InputTextModule, FloatLabelModule],
   templateUrl: './analyse-cv.component.html',
   styleUrl: './analyse-cv.component.css'
 })
