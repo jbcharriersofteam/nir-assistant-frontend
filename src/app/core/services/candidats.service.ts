1a9ba5e3b6b983b0e92dfcbf4b2c46bdfa7a0b8f
jbcharrier <jbcharrier33@gmail.com>
2024-09-30 11:19:43 +0200

    upload cv api

--- src/app/core/services/candidats.service.ts
+++ src/app/core/services/candidats.service.ts
@@ -1,25 +1,36 @@
-import { Injectable } from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CandidatService {
+  private API: string =
+    'https://v7sk3jpn45.execute-api.eu-west-3.amazonaws.com/v1/';
 
-  private API: string = "https://v7sk3jpn45.execute-api.eu-west-3.amazonaws.com/v1/"
-
-  constructor(private http: HttpClient) { }
+  body: any;
+  constructor(private http: HttpClient) {}
 
   getCvList(): Observable<any> {
     return this.http.get<any>(this.API + 'cv-list');
   }
-  uploadCv( data: any){
-    return this.http.post<any>(this.API + 'upload-cv', data);
+
+  uploadCv(data: any) {
+    const reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = () => {
+      const base64File = reader.result?.toString().split(',')[1];
+      this.body = {
+        file: base64File,
+        fileName: data.name,
+      };
+      console.log(this.body);
+    };
+    return this.http.post<any>(this.API + 'upload-cv', this.body);
   }
+
   getAllAnalysedCandidats(): Observable<any> {
     return this.http.get<any>(this.API + 'scan-cv-table');
   }
 }
-
-
